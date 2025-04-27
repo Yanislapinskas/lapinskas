f136b1cc4612867133ef3a1682d9bac2ba7f0894
Yanislapinskas <yanislapiskas20@mail.ru>
2025-04-27 20:43:25 +0300

    first commit

---
+++ src/components/EssentialLink.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-item
+    clickable
+    tag="a"
+    target="_blank"
+    :href="p.link"
+  >
+	 <q-item-section>
+      <q-item-label>{{ p.title }}</q-item-label>
+      <q-item-label caption v-if="p.caption!=''"> 
+		<q-chip class="glossy" color="orange" text-color="white" icon-right="star">
+			{{ p.caption }}
+		</q-chip>
+	  </q-item-label>
+    </q-item-section>
+	
+    <q-item-section
+      v-if="p.icon"
+      avatar
+    >
+      <q-icon :name="p.icon" />
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup>
+const p  = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+
+  caption: {
+    type: String,
+    default: ''
+  },
+
+  link: {
+    type: String,
+    default: '#'
+  },
+
+  icon: {
+    type: String,
+    default: ''
+  }
+})
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img :src="logo">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+		<q-btn outline color="white" icon="login">
+		Log in
+		</q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script setup>
+import logo from 'src/assets/logo.png';
+</script>
