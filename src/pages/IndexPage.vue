6e410409a5d6ebac0733b3813245f205c3eebdf2
Yanislapinskas <yanislapiskas20@mail.ru>
2025-05-11 18:20:36 +0300

    deepseek размер

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="row">
-    <q-card class="col-4">
-      <q-img height="200" src="https://avatars.mds.yandex.net/get-feeds-media/5396768/847c53e4539489421bfdfe803b4385ac78dbbddb/orig">
+  <div class="row q-gutter-md">
+    <q-card class="col-4" style="height: 200px;">
+      <q-img 
+        height="100%"
+        src="https://avatars.mds.yandex.net/get-feeds-media/5396768/847c53e4539489421bfdfe803b4385ac78dbbddb/orig"
+      >
         <div class="absolute-bottom text-h6">
           Title
         </div>
       </q-img>
-	  </q-card>
-	  <q-card class="col-4">
-      <q-img height="200" src="https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop	">
+    </q-card>
+    <q-card class="col-4" style="height: 200px;">
+      <q-img 
+        height="100%"
+        src="https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop"
+      >
         <div class="absolute-bottom text-h6">
           Title
         </div>
       </q-img>
-	  </q-card>
+    </q-card>
   </div>
 </template>
 
