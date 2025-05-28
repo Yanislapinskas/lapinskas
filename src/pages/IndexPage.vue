0feebb8fbf28b77d070664aae92b5522f0d5b2a3
Yanislapinskas <yanislapiskas20@mail.ru>
2025-05-28 14:39:07 +0300

    Много карт

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -13,7 +13,29 @@
         </div>
       </q-img>
     </q-card>
+	
     <q-card class="col-4" style="height: 200px;">
+      <q-img 
+        height="100%"
+        src="https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop"
+      >
+        <div class="absolute-bottom text-h6">
+          Title
+        </div>
+      </q-img>
+    </q-card>
+	
+	<q-card class="col-4" style="height: 200px;">
+      <q-img 
+        height="100%"
+        src="https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop"
+      >
+        <div class="absolute-bottom text-h6">
+          Title
+        </div>
+      </q-img>
+    </q-card>
+	<q-card class="col-4" style="height: 200px;">
       <q-img 
         height="100%"
         src="https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop"
