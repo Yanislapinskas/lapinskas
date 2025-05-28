8b6df8422951269bbd47906404951127d80edc2d
Yanislapinskas <yanislapiskas20@mail.ru>
2025-05-28 14:57:49 +0300

    цикл

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,51 +3,27 @@
 	Бесплатная доставка
   </h3>
   <div class="row q-ma-xs">
-    <q-card class="col-4" style="height: 200px;">
+    <q-card v-for="(image,index) in images" :key="index" class="col-4" style="height: 200px;">
       <q-img 
         height="100%"
-        src="https://avatars.mds.yandex.net/get-feeds-media/5396768/847c53e4539489421bfdfe803b4385ac78dbbddb/orig"
+        :src="image"
       >
         <div class="absolute-bottom text-h6">
-          Title
+          Title {{index}}
         </div>
       </q-img>
     </q-card>
 	
-    <q-card class="col-4" style="height: 200px;">
-      <q-img 
-        height="100%"
-        src="https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop"
-      >
-        <div class="absolute-bottom text-h6">
-          Title
-        </div>
-      </q-img>
-    </q-card>
-	
-	<q-card class="col-4" style="height: 200px;">
-      <q-img 
-        height="100%"
-        src="https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop"
-      >
-        <div class="absolute-bottom text-h6">
-          Title
-        </div>
-      </q-img>
-    </q-card>
-	<q-card class="col-4" style="height: 200px;">
-      <q-img 
-        height="100%"
-        src="https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop"
-      >
-        <div class="absolute-bottom text-h6">
-          Title
-        </div>
-      </q-img>
-    </q-card>
   </div>
 </template>
 
 <script setup>
-//
+import { ref } from 'vue'
+
+const images = ref([
+"https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop",
+"https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop",
+"https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop",
+"https://avatars.mds.yandex.net/get-feeds-media/5396768/847c53e4539489421bfdfe803b4385ac78dbbddb/orig",
+])
 </script>
