d9cab2aec938106a4a22d804741a6959e3945b34
Yanislapinskas <yanislapiskas20@mail.ru>
2025-05-31 14:48:58 +0300

    fix14

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,43 +2,25 @@
   <div>
     <h3 class="q-ma-xs">Бесплатная доставка</h3>
     <div class="row q-ma-xs">
-      > 
-
-	>
-<q-carousel
-      v-model="slide"
-      transition-prev="slide-right"
-      transition-next="slide-left"
-      animated
-      control-color="primary"
-      class="rounded-borders"
-    >
-      <q-carousel-slide name="style" class="column no-wrap flex-center">
-        <q-icon name="style" color="primary" size="56px" />
-        <div class="q-mt-md text-center">
-          {{ lorem }}
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="tv" class="column no-wrap flex-center">
-        <q-icon name="live_tv" color="primary" size="56px" />
-        <div class="q-mt-md text-center">
-          {{ lorem }}
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="layers" class="column no-wrap flex-center">
-        <q-icon name="layers" color="primary" size="56px" />
-        <div class="q-mt-md text-center">
-          {{ lorem }}
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide name="map" class="column no-wrap flex-center">
-        <q-icon name="terrain" color="primary" size="56px" />
-        <div class="q-mt-md text-center">
-          {{ lorem }}
-        </div>
-      </q-carousel-slide>
-    </q-carousel>
-
+      <q-carousel
+        v-model="slide"
+        transition-prev="slide-right"
+        transition-next="slide-left"
+        animated
+        control-color="primary"
+        navigation
+        class="rounded-borders"
+        height="300px"
+      >
+        <q-carousel-slide
+          v-for="(img, index) in images"
+          :key="index"
+          :name="index"
+          class="column no-wrap flex-center"
+        >
+          <q-img :src="img" :alt="'Slide ' + index" style="max-height: 100%; max-width: 100%;" />
+        </q-carousel-slide>
+      </q-carousel>
     </div>
   </div>
 </template>
@@ -49,7 +31,8 @@ import { ref } from 'vue';
 const images = ref([
   "https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop",
   "https://avatars.mds.yandex.net/get-feeds-media/5396768/847c53e4539489421bfdfe803b4385ac78dbbddb/orig",
+  "https://placekitten.com/400/300", // дополнительное для теста
 ]);
-const slide = ref(0 );
-const lorem = ref("test");
+
+const slide = ref(0); // используем index как name
 </script>
