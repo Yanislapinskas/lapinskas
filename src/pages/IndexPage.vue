59ef003486bdad64f3f3d8b74e99d098cea5816b
Yanislapinskas <yanislapiskas20@mail.ru>
2025-05-31 13:19:07 +0300

    fix

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,27 +5,21 @@
   <div class="row q-ma-xs">
   
     <q-carousel
-      v-model="slide"
-      transition-prev="slide-right"
-      transition-next="slide-left"
-      animated
-      control-color="primary"
-      class="rounded-borders"
-    >
-      <q-carousel-slide v-for="(image,index) in images" :key="index" name="style" class="column no-wrap flex-center">
-	  
-		<q-card  class="col-4" style="height: 200px;">
-		  <q-img 
-			height="100%"
-			:src="image"
-		  >
-			<div class="absolute-bottom text-h6">
-			  Title {{index}}
-			</div>
-		  </q-img>
-		</q-card>
-	  </q-carousel-slide>
-    </q-carousel>
+  v-model="slide"
+  transition-prev="slide-right"
+  transition-next="slide-left"
+  animated
+  control-color="primary"
+  class="rounded-borders"
+>
+  <q-carousel-slide
+    v-for="(image, index) in images"
+    :key="index"
+    :name="index"
+  >
+    <q-img :src="image" class="fit" />
+  </q-carousel-slide>
+</q-carousel>
   </div>
 </template>
 
@@ -43,4 +37,5 @@ const images = ref([
 "https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop",
 "https://avatars.mds.yandex.net/get-eda/2353725/2dc822395bf1d344702e8626e1f7a44f/400x400nocrop",
 ])
+const slide = ref(0s)
 </script>
